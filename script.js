6cc8a788b6b9dc2af36bf13f75e49fb068f7cf65
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-10 14:17:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function mincost(arr)
-{ 
+function mincost(arr) { 
 //write your code here
 // return the min cost
-  
+  arr.sort((a, b) => a - b);
+
+	let cost = 0;
+	for (let i = 0; i < arr.length; i++) {
+		const min1 = arr.shift();
+		const min2 = arr.shift();
+
+		const currentCost = min1 + min2;
+
+		cost += currentCost;
+
+		arr.push(currentCost);
+	}
+	return cost;
 }
 
 module.exports=mincost;
